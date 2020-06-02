9eac3239dbccb5687ce3e3e6b9c471758acfdd05
nikbreder <64217083+nikbreder@users.noreply.github.com>
2020-06-02 13:03:59 +0600

    Add files via upload

---
+++ classes/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+abstract class Config
+{
+    const HOST = 'localhost';
+    const DB_NAME = 'h150220_ssdsd';
+    const DB_USER = 'h150220_nikita';
+    const DB_PASSWORD = 'Y4f2F4f7';
+
+}
---
+++ classes/UserMap.php
@@ -0,0 +1,5 @@
+<?php
+
+class UserMap extends BaseMap {
+
+}
